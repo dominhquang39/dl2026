971496c2d9e7656646a050933b071ab6d2185890
Do Minh Quang <minhquangdo19@gmail.com>
2026-05-06 20:25:31 +0700

    Add experiments with multiple lr choice

--- gradient_descent.py
+++ gradient_descent.py
@@ -7,7 +7,7 @@ def df(x):
 def gradient_descent(f, df, x0, lr, iter):
     x = x0
 
-    print(f"Initial guess: {x}")
+    print(f"Initial guess: {x}, learning rate: {lr}")
 
     for i in range(iter):
         fx = f(x)
@@ -15,7 +15,10 @@ def gradient_descent(f, df, x0, lr, iter):
         grad = df(x)
         x -= lr*grad
 
-gradient_descent(f, df, x0=5, lr=0.1, iter=20)
+if __name__ == '__main__':
+    learning_rate = [0.001, 0.005, 0.1, 0.5]
+    for lr in learning_rate:
+        gradient_descent(f, df, x0=5, lr=lr, iter=20)
 
 
 
