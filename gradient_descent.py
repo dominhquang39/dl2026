92b5eab03251a82e429792b6b463c6fdf7f2aa14
Do Minh Quang <minhquangdo19@gmail.com>
2026-05-06 20:42:31 +0700

    Report for labwork 1

--- gradient_descent.py
+++ gradient_descent.py
@@ -9,11 +9,20 @@ def gradient_descent(f, df, x0, lr, iter):
 
     print(f"Initial guess: {x}, learning rate: {lr}")
 
-    for i in range(iter):
+    # for i in range(iter):
+    #     fx = f(x)
+    #     print(f"Iteration {i+1}: x={x:.3f}, f(x)={fx:.3f}")
+    #     grad = df(x)
+    #     x -= lr*grad
+
+    count = 0
+    while f(x) != 0.0:
         fx = f(x)
-        print(f"Iteration {i+1}: x={x:.3f}, f(x)={fx:.3f}")
+        # print(f"x={x:.3f}, f(x)={fx:.3f}")
         grad = df(x)
         x -= lr*grad
+        count += 1
+    print(f"Converged to minimum at x={x:.3f} after {count} iterations\n")
 
 if __name__ == '__main__':
     learning_rate = [0.001, 0.005, 0.1, 0.5]
